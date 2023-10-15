05ffbe99bc89c299cd2f3cd6b2a077e472753426
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-10-15 16:03:52 +0200

    ADD - Login controller

---
+++ service/controllers/loginController.ts
@@ -0,0 +1,56 @@
+import { Response, Request } from "express";
+
+import { findUser } from "../dataService/findUser";
+import { UserModel } from "../../domain/models/userModel";
+import { passwordHasher } from "../../application/passwordHasher/passwordHasher";
+
+interface LoginViewModel { 
+    email:string,
+    password:string
+}
+
+export async function loginValidation(credentials: LoginViewModel, userFromDb : UserModel){
+    const hashPassword = await passwordHasher(credentials.password);
+
+    if (hashPassword !== userFromDb.password){ 
+        console.log("Invalid Password!")
+        return false
+    }
+
+    return true
+
+};
+
+
+export function messageGenerator(message:string){ 
+    interface ResponseMessage{ 
+        message:string
+    }
+    return {message} as ResponseMessage
+}
+
+
+export async function loginController (req:Request, res:Response){ 
+    try{
+        const credentials = req.body as LoginViewModel;
+
+        const user = await findUser(credentials.email);
+        if(!user) {
+            res.status(403).json({message: "Invalid username or password"})
+            return
+        }
+        const isUserValid = await loginValidation(credentials, user);
+        if(!isUserValid) {
+            res.status(403).json(messageGenerator("Invalid username or password!"))
+            return
+        }
+
+
+
+
+
+    }catch(err:Error| unknown){
+        console.log(err)
+        res.status(500).json({message:"Error on log in"})
+    }
+}
