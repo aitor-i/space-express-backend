6ab18fbf7fc25bf5c57fe2c0d09458dcd1495706
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-10-18 07:49:06 +0200

    ADD - Token method is expired

---
+++ application/isTokenExpired/isTokenExpired.ts
@@ -0,0 +1,7 @@
+import Token from "../../domain/token/token";
+
+export function isTokenExpired(tokenToValidate:string){ 
+
+    const token = new Token();
+    return token.isExpired(tokenToValidate);
+}
