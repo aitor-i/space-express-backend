bd684ddb0ac20febb20b41c956374af67f415f79
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-10-18 06:20:26 +0200

    ADD - Generate token

---
+++ application/generateToken/generateToken.ts
@@ -0,0 +1,8 @@
+import Token from "../../domain/token/token";
+
+export function generateToken(email:string){ 
+
+    const token = new Token();
+    token.generateToken(email);
+    return token.getToken();
+}
