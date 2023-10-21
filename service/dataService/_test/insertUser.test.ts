ee17561a4f082958d56c75401cf0e4fff5923667
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-10-21 07:23:08 +0200

    DEL - try catch

--- service/dataService/_test/insertUser.test.ts
+++ service/dataService/_test/insertUser.test.ts
@@ -18,21 +18,17 @@ describe("User in DB", () => {
     expect(dbResponse.isValid).toBe(true);
   });
 
-  it("Should find user", async () => {
-    const testUser = {
-      username: "TEST_NAME",
-      password: "TEST_PASS",
-      email: "TEST@TEST.TS",
-    } as UserModel;
+    it("Should find user", async () => {
+        const testUser = {
+            username: "TEST_NAME",
+            password: "TEST_PASS",
+            email: "TEST@TEST.TS",
+        } as UserModel;
 
-    try {
-      const dbResponse = await insetUser(testUser);
-      const userFromDb = await findUser(testUser!.email!);
+        const dbResponse = await insetUser(testUser);
+        const userFromDb = await findUser(testUser!.email!);
 
-      expect(userFromDb?.username).toBe(testUser.username);
-    } catch {
-    } finally {
-      deleteUser(testUser!.email!);
-    }
-  });
+        deleteUser(testUser!.email!);
+        expect(userFromDb?.username).toBe(testUser.username);
+    });
 });
