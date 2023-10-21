b75fa22c1c13aaa8a1ff983cc73f70d5c014f780
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-10-21 07:23:32 +0200

    ADD - Better error messages

--- service/dataService/insertUser.ts
+++ service/dataService/insertUser.ts
@@ -4,26 +4,33 @@ interface UserDocument extends Document, UserModel {}
 import { UserModel } from "../../domain/models/userModel";
 
 export async function insetUser(user: UserModel) {
-  try {
-    await mongoClient.connect();
-    const userPointer = getPointer(usersCollection);
+    try {
+        await mongoClient.connect();
+        const userPointer = getPointer(usersCollection);
 
-    const isUser = await userPointer.findOne({ email: user.email });
-    if (isUser) {
-      console.log(`Email ${user.email} is taken!`);
-        return {isValid: false, message:"Email is taken!"}
+        console.log("CONNECTED!")
+
+        const isUser = await userPointer.findOne({ email: user.email });
+        if (isUser) {
+            console.error(`Email ${user.email} is taken!`);
+            return {isValid: false, message:"Email is taken!"}
+        }
+
+        const dbResponse = await userPointer.insertOne(user);
+        if (!dbResponse.acknowledged) {
+            console.error("DB response: ", dbResponse)
+            throw new Error("Error on user  db");
+        }
+        return {isValid:true, message:"User inseted"};
+
+    } catch (error: Error | unknown) {
+        console.error("Error adding user", Error);
+        throw new Error("Error inserting user");
+
+    } finally {
+        setTimeout(async () => {
+            await mongoClient.close();
+            console.log("Db closed");
+        }, 4000);
     }
-    const dbResponse = await userPointer.insertOne(user);
-    if (!dbResponse.acknowledged) throw new Error("Error on user  db");
-
-    return {isValid:true, message:"User inseted"};
-  } catch (error: Error | unknown) {
-    console.log(Error.toString());
-    throw new Error("Error inserting user");
-  } finally {
-    setTimeout(async () => {
-      await mongoClient.close();
-      console.log("Db closed");
-    }, 4000);
-  }
 }
