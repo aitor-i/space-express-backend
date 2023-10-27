14f80f874d7b8c3d9594f4ce9612dab124f640b6
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-10-27 18:01:10 +0200

    ADD - Get seats controller

---
+++ service/controllers/selectSeat/getSeatsController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+
+import { messageGenerator } from "../../messageGenerator/messageGenerator";
+import { getSeatsById } from "../../dataService/seatsDb/getSeatsById";
+
+export async function getSeatsController(req:Request, res:Response){ 
+    try{ 
+        const flightId = req.get("id")
+        if(!flightId){ 
+            res.status(404).json(messageGenerator("no id found"))
+            return
+        }
+        const seats = await getSeatsById(flightId)
+
+        if(seats.length == 0){ 
+            res.status(404).json(messageGenerator("Not fligths found"))
+            return
+        }
+
+        res.json({...messageGenerator("Ok"), seats})
+
+    }catch(err:Error|unknown){ 
+        res.status(500).json(messageGenerator("Error finding seats  "))
+    }
+}
--- service/routers/selectSeatRouter.ts
+++ service/routers/selectSeatRouter.ts
@@ -1,5 +1,6 @@
 import { Router, Response, Request } from "express";
 import { authMiddleware } from "../middleware/authMiddleware";
+import { getSeatsController } from "../controllers/selectSeat/getSeatsController";
 
 export const selectSeatRouter = Router();
 
@@ -7,12 +8,4 @@ selectSeatRouter.get("/", (req: Request, res: Response) => {
   res.status(200).json({ message: "Select seat service is up running" });
 });
 selectSeatRouter.use(authMiddleware);
-selectSeatRouter.get("/td", (req:Request, res:Response)=>{ 
-    try{
-       res.status(200).send({})
-    
-    }catch(err:Error|unknown){ 
-        console.error(err)
-        res.status(500).json({message:"error"})
-    }
-})
+selectSeatRouter.get("/getSeats", getSeatsController )
