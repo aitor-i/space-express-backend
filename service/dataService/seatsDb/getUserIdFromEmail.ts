5832d4c352370617c7ed50f187537e9ee6cf3d93
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-11-02 20:05:18 +0100

    ADD - Get user id from email

---
+++ service/dataService/seatsDb/getUserIdFromEmail.ts
@@ -0,0 +1,29 @@
+import { usersCollection, mongoClient, getPointer } from './../mongoClient';
+
+import { Document, WithId } from 'mongodb';
+import { UserModel } from '../../../domain/models/userModel';
+
+interface UserDocument extends Document, UserModel {}
+
+export async function getUserIdFromEmail(email:string){ 
+    try{ 
+    await mongoClient.connect()
+        const userPointer = getPointer(usersCollection);
+
+        console.log("Email",email)
+
+        const userFromDb = await userPointer.findOne({ email:email} as Partial<UserModel> ) as WithId<UserDocument>
+        console.log("user from db", userFromDb)
+        if(userFromDb === null ) return null
+
+        return userFromDb._id
+
+    }catch(err){ 
+        console.error(err)
+        throw new Error("Error finding user id!")
+    }finally{ 
+        mongoClient.close();
+        console.log('Db closed!!');
+    }
+
+}
