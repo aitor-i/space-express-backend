48a2ad71b4bfbde5ee6e44211aff82bc7693731e
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-11-10 16:58:50 +0100

    ADD - get ips by id

---
+++ service/dataService/ipServicesDB/getIpsByIpDb.ts
@@ -0,0 +1,30 @@
+import { Document, WithId } from 'mongodb';
+import { ipCollection, mongoClient, getPointer } from './../mongoClient';
+import { IpModel } from './insertIp';
+
+interface IpDocument extends IpModel, Document {}
+
+
+export async function getIpsByIpDb(ip: string){ 
+    try{
+        await mongoClient.connect()
+        const ipPointer = getPointer(ipCollection)
+
+        const ips = await ipPointer.find({ip} as Partial<IpModel>).toArray() as WithId<IpDocument>[]
+        const mappedIp = ips.map((ipDocument)=>{return {ip: ipDocument.ip, requestDate:ipDocument.requestDate } as IpModel } )
+
+        mongoClient.close()
+        return mappedIp
+
+    }catch(err){
+
+        console.error(err)
+        false
+    }finally{ 
+        setTimeout(()=>{ 
+            mongoClient.close()
+            console.log("ip db closed!")
+        }, 3000)
+    }
+
+}
