913089e8a09ac20d9ad352ffb68fe83f33f7f316
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-11-11 16:30:13 +0100

    UPDATE - Is token expired

--- application/isTokenExpired/isTokenExpired.ts
+++ application/isTokenExpired/isTokenExpired.ts
@@ -1,6 +1,10 @@
 import Token from '../../domain/token/token';
 
 export function isTokenExpired(tokenToValidate: string) {
+    try {
     const token = new Token();
     return token.isExpired(tokenToValidate);
+    }catch{ 
+        return true
+    }
 }
