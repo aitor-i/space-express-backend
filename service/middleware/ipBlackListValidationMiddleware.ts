436352c6d693b22eb352a07ce021c8aadd03997c
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-11-12 19:37:15 +0100

    ADD - Ip black list validation

---
+++ service/middleware/ipBlackListValidationMiddleware.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction} from "express"
+import { findIpInBlackList } from "../dataService/ipServicesDB/fidIpInBlackListDb"
+import { messageGenerator } from "../messageGenerator/messageGenerator";
+
+
+export async function ipBlackListValidationMiddleware(req:Request, res:Response, next:NextFunction){ 
+    try{ 
+        const ip = req.ip
+
+        const isIpOnBlackList = await findIpInBlackList(ip);
+
+        if(isIpOnBlackList){
+            
+
+        res.status(400).json(messageGenerator("your ip is on a black list!"))
+            return
+        }
+        else{
+            
+        
+        next()
+        }
+
+    }catch(err){ 
+        console.error(err)
+        res.status(500).json(messageGenerator("errror validating ip!"))
+    }
+
+}
