310df6ca12a974f6c6511fa2e66513e7389ce72a
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-11-12 19:39:29 +0100

    ADD - Validations to login ruters

--- service/routers/loginRouter.ts
+++ service/routers/loginRouter.ts
@@ -3,6 +3,7 @@ import { registerController } from '../controllers/registerController';
 import { loginController } from '../controllers/loginController';
 import { validateTokenController } from '../controllers/validateTokenController';
 import { changePasswordController } from '../controllers/changePasswordController';
+import { ipRegistrationMiddleware } from '../middleware/ipRegistretionMiddleware';
 
 export const loginRouter = Router();
 
@@ -10,7 +11,8 @@ loginRouter.get('/', (req: Request, res: Response) => {
     res.status(200).json({ message: 'Service running' });
 });
 
+loginRouter.post('/validate-token', validateTokenController);
+loginRouter.use(ipRegistrationMiddleware)
 loginRouter.post('/register', registerController);
 loginRouter.post('/login', loginController);
-loginRouter.post('/validate-token', validateTokenController);
 loginRouter.post('/change-password', changePasswordController);
