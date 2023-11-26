e94a6e34100b3fdf550483b63145d308f1ef198d
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-11-26 14:05:29 +0100

    ADD - Basic chat functionality

--- chat/service/runChatServer.ts
+++ chat/service/runChatServer.ts
@@ -5,11 +5,43 @@ export function runChatServer (server: http.Server, client: Set<WebSocket> ) {
     const wss  = new WebSocketServer({server})
 
     wss.on("connection", (ws:WebSocket)=> { 
-        console.log("connection!")
+        
+        console.log("WS: ", ws)
+
+        // Get flight data 
+        // Fetch prev data from DB
+        const prevMessages:string[] =  [];
+        client.add(ws)
+
+        // Send de prev data as first message
+        ws.send(JSON.stringify(prevMessages))
+        
+        ws.on("message", (message:string)=>{ 
+            const parsedMessage = JSON.parse(message);
+            console.log(parsedMessage);
+
+            prevMessages.push(parsedMessage);
+
+            ws.send(parsedMessage.message)
+        })
+
+
+        ws.on("close", ()=> { 
+            // Persist data on BD
+    
+            console.log("chat closed!!", prevMessages)
+        })
+
+
 
         
         
     })
 
+
+    wss.on("error", (err)=>{ 
+        console.log(err)
+
+    })
 
 }
