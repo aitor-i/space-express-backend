41a977999ef45be1890ab298f0f1d750f4ca93d9
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-12-04 15:40:33 +0100

    ADD - message and response interfaces on chat

--- chat/service/runChatServer.ts
+++ chat/service/runChatServer.ts
@@ -1,5 +1,21 @@
 import http from 'http'
 import {WebSocket, WebSocketServer} from 'ws'
+ 
+interface Message { 
+    status: 'messeag' | 'typeing',
+    message: string|undefined,
+    userId:string,
+    userAlias: string
+    roomId: string
+}
+
+interface Response { 
+    status: 'messeag' | 'typeing',
+    messages: Message[]|undefined,
+    userId:string,
+    userAlias: string
+    roomId:string
+}
 
 export function runChatServer (server: http.Server, client: Set<WebSocket> ) { 
     const wss  = new WebSocketServer({server})
@@ -10,19 +26,37 @@ export function runChatServer (server: http.Server, client: Set<WebSocket> ) {
 
         // Get flight data 
         // Fetch prev data from DB
-        const prevMessages:string[] =  [];
+        const rooomId = ws.protocol;
+        const prevMessages:Message[] =  [];
         client.add(ws)
 
         // Send de prev data as first message
         ws.send(JSON.stringify(prevMessages))
         
         ws.on("message", (message:string)=>{ 
-            const parsedMessage = JSON.parse(message);
+            const parsedMessage = JSON.parse(message) as Message;
             console.log(parsedMessage);
 
-            prevMessages.push(parsedMessage);
+            if(parsedMessage.status === 'typeing'){ 
+                const response: Response = { 
+                    status: 'typeing',
+                    messages: undefined,
+                    userId: parsedMessage.userId,
+                    userAlias: parsedMessage.userAlias,
+                    roomId: parsedMessage.roomId
+                }
 
-            ws.send(parsedMessage.message)
+                const responseString = JSON.stringify(response)
+                ws.send(responseString)
+                return
+            }
+
+            prevMessages.push(parsedMessage);
+            
+            const textMessages = prevMessages.filter(message => message.message !== undefined)
+            const textMessagesString = JSON.stringify(textMessages)
+
+            ws.send(textMessagesString)
         })
 
 
